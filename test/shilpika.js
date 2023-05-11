d744999fae1b31f7a323e3c40fbc102db2f4a9c1
Shilpa <knsl.sree@gmail.com>
2023-05-11 21:41:41 +0200

    Revised

--- test/shilpika.js
+++ test/shilpika.js
@@ -1,3 +1,4 @@
+
 // Includes
 const { Builder , By, Key, until} = require ('selenium-webdriver');
 const should = require('chai').should();
@@ -11,7 +12,7 @@ so that i can find the product that matches my preferences or needs.
 describe.only('Search for a product of particular color', () => {
     //Test case:
     context('Given a user is on the search page',() => {
-        it('should show the shirt of specified color'), async() => {
+        it('should show the shirt of preferred color'), async() => {
             //start the webbrowser
             const driver = await new Builder().forBrowser('firefox').build();
 
@@ -22,20 +23,20 @@ describe.only('Search for a product of particular color', () => {
             //Get the search input
             await driver.wait(until.elementLocated(By.css('#search')),50000);
             await driver.findElement(By.id('search')).sendKeys('shirt for men', Key.RETURN);
-            await driver.findElement(By.id('search')).sendKeys('orange',Key.RETURN);
+           
           
           
 
             //Find the first product
              await driver.wait(until.elementsLocated(By.css('.item.product.product-item:nth-child(3)'))),10000;
             const product= await driver.findElement(By.css('.item.product.product-item:nth-child(3)'));
-           
+            
            
 
             //find the information in the product we selected
              let productTitle = await product.findElement(By.css('.product-item-link'));
              let productPrice = await product.findElement(By.css('.price'))
-             let productColor = await product.findElement(By.id('#option-label-color-93-item-56'));
+             let productColor = await product.findElement(By.class('.swatch-option color'));
             
 
             // Extra text
@@ -45,12 +46,8 @@ describe.only('Search for a product of particular color', () => {
 
             productTitleText.should.equal('Balboa Persistence Tee');
             productPriceText.should.equal('$29.00');
-            productColorText.should.equal('orange');
+            productColorText.should.equal('green');
 
-           
-      
-            // Find the orange color option and select it
-            driver.findElement(By.id('#option-label-color-93-item-56> option:nth-child(3)')).click();
         
             console.log(productTitleText, productPriceText, productColorText);
           
